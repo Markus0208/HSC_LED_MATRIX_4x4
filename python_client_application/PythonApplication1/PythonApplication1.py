bf84cea28ce8c27c016d09f0e73cda70b75d2d73
Markus0208 <markus.schmidhuber@st.oth-regensburg.de>
2025-02-09 11:26:32 +0100

    final version, fixed position problem, fixed python single send, clean up

---
+++ python_client_application/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,4 @@
+﻿from frontend import run_app
+         
+if __name__ == "__main__":
+    run_app()
---
+++ vitis_software/Wrapper_Ch3_Correct_ETH/platform.tcl
@@ -0,0 +1,22 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\thego\Desktop\HSC_PNM\vitis_software\Wrapper_Ch3_Correct_ETH\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\thego\Desktop\HSC_PNM\vitis_software\Wrapper_Ch3_Correct_ETH\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {Wrapper_Ch3_Correct_ETH}\
+-hw {C:\Users\thego\Desktop\HSC_PNM\Wrapper_Ch3_Correct_ETH.xsa}\
+-fsbl-target {psu_cortexa53_0} -out {C:/Users/thego/Desktop/HSC_PNM/vitis_software}
+
+platform write
+domain create -name {freertos10_xilinx_ps7_cortexa9_0} -display-name {freertos10_xilinx_ps7_cortexa9_0} -os {freertos10_xilinx} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {freertos_lwip_echo_server}
+platform generate -domains 
+platform active {Wrapper_Ch3_Correct_ETH}
+domain active {zynq_fsbl}
+domain active {freertos10_xilinx_ps7_cortexa9_0}
+platform generate -quick
+platform generate
